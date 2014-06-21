cdf4ab83beb4606d3ecd76e3aa20eca476ef31f4
Ma Eikei <maeikei@gmail.com>
2014-06-21 12:53:56 +0900

    add cx921 build system

--- cx921.mk
+++ cx921.mk
@@ -1,2 +1,6 @@
 all:
-	
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- rk3188_cx921_defconfig
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j4
+	rm -rf ./modules && mkdir -p ./modules
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules -j4 
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=./modules modules_install -j4
